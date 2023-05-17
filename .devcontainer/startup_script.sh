139d308b7da5a9737883f8efe990ed7cc9547903
mbowen <mike.bowen@blackrock.com>
2023-05-17 09:25:09 -0400

    feat: scaffold playground

---
+++ .devcontainer/startup_script.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+sudo apt-get update -y
+
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+source ~/.bashrc 
+
+echo "✅ brew installed successfully"
+
+brew install derailed/k9s/k9s
+
+sudo apt-get install fzf -y
+
+echo "✅ kubectx, kubens, fzf, homebrew, and k9s installed successfully"
+
+alias k="kubectl"
+alias kga="kubectl get all"
+alias kgn="kubectl get all --all-namespaces"
+alias kdel="kubectl delete"
+alias kd="kubectl describe"
+alias kg="kubectl get"
+
+echo 'alias k="kubectl"' >> /home/$USER/.bashrc
+echo 'alias kga="kubectl get all"' >> /home/$USER/.bashrc
+echo 'alias kgn="kubectl get all --all-namespaces"' >> /home/$USER/.bashrc
+echo 'alias kdel="kubectl delete"' >> /home/$USER/.bashrc
+echo 'alias kd="kubectl describe"' >> /home/$USER/.bashrc
+echo 'alias kg="kubectl get"' >> /home/$USER/.bashrc
+
+echo "✅ The following aliases were added:"
+echo "alias k=kubectl"
+echo "alias kga=kubectl get all"
+echo "alias kgn=kubectl get all --all-namespaces"
+echo "alias kdel=kubectl delete"
+echo "alias kd=kubectl describe"
+echo "alias kg=kubectl get"
+
+source ~/.bashrc 
---
+++ README.md
@@ -0,0 +1,20 @@
+# Overview
+
+A Minikube based Kubernetes Playground for GitHub Codespaces that leverages devcontainers to ignite.
+
+## Instructions
+
+Simply fork the repo and launch a new codespace from the forked repo.
+
+## What you will get
+
+1. Access to a Linux terminal with `kubectl`, `helm`, `k9s`, `kubectx`, and `kubens` already installed.
+2. A Minikube instance will automatically start, this takes about 5 minutes so please be patient!
+3. Also included is a list of aliases to use:
+
+- alias k="kubectl"
+- alias kga="kubectl get all"
+- alias kgn="kubectl get all --all-namespaces"
+- alias kdel="kubectl delete"
+- alias kd="kubectl describe"
+- alias kg="kubectl get"
